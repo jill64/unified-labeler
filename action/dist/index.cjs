38d88f84738c02e1bc2e438293e4832bec08e5ee
jill64 <intents.turrets0h@icloud.com>
2024-02-08 06:06:31 +0900

    def action

--- action/dist/index.cjs
+++ action/dist/index.cjs
@@ -23014,7 +23014,94 @@ Cause on Action
 };
 
 // action/src/index.ts
-action(async () => {
+action(async ({ payload, octokit }) => {
+  const { owner, repo, data } = payload;
+  const { data: allRepo } = await octokit.rest.repos.listForUser({
+    username: owner
+  });
+  class Labeler {
+    repo;
+    owner;
+    label;
+    constructor(repo2, owner2, label) {
+      this.repo = repo2;
+      this.owner = owner2;
+      this.label = label;
+    }
+    create() {
+      return octokit.rest.issues.createLabel({
+        owner: this.owner,
+        repo: this.repo,
+        name: this.label.name,
+        color: this.label.color,
+        description: this.label.description ?? ""
+      });
+    }
+    update() {
+      return octokit.rest.issues.createLabel({
+        owner: this.owner,
+        repo: this.repo,
+        name: this.label.name,
+        color: this.label.color,
+        description: this.label.description ?? ""
+      });
+    }
+    delete() {
+      return octokit.rest.issues.deleteLabel({
+        owner: this.owner,
+        repo: this.repo,
+        name: this.label.name
+      });
+    }
+  }
+  if (data.type === "repo_created") {
+    const [{ data: base }, { data: labels }] = await Promise.all([
+      await octokit.rest.issues.listLabelsForRepo({
+        owner,
+        repo
+      }),
+      await octokit.rest.issues.listLabelsForRepo({
+        owner: data.owner,
+        repo: data.repo
+      })
+    ]);
+    await Promise.all(
+      base.map(async (label) => {
+        const labeler = new Labeler(data.repo, data.owner, label);
+        await (labels.includes(label) ? labeler.update() : labeler.create());
+      })
+    );
+    const { data: newLabels } = await octokit.rest.issues.listLabelsForRepo({
+      owner: data.owner,
+      repo: data.repo
+    });
+    await Promise.all(
+      newLabels.map(async (label) => {
+        if (!base.includes(label)) {
+          const labeler = new Labeler(data.repo, data.owner, label);
+          await labeler.delete();
+        }
+      })
+    );
+    return;
+  }
+  const result = allRepo.filter((x) => x.name !== data.repo).map(async (repo2) => {
+    const { data: labels } = await octokit.rest.issues.listLabelsForRepo({
+      owner: repo2.owner.login,
+      repo: repo2.name
+    });
+    const label = new Labeler(repo2.name, repo2.owner.login, data.label);
+    if (data.type === "deleted") {
+      return await label.delete();
+    }
+    if (data.type === "created") {
+      return await label.create();
+    }
+    if (data.type === "edited") {
+      await (labels.includes(data.label) ? label.update() : label.create());
+    }
+  });
+  await Promise.allSettled(result);
 });
 /*! Bundled license information:
 
