5850c9c4612d7940633e36e6eced1d026de8dbab
jill64 <contact@jill64.dev>
2025-03-10 14:18:07 +0900

    chore: update build process and dependencies; remove unused build script

--- action/scripts/build.ts
+++
@@ -1,9 +0,0 @@
-import { build } from 'esbuild'
-
-build({
-  entryPoints: ['action/src/index.ts'],
-  bundle: true,
-  platform: 'node',
-  target: 'node20',
-  outfile: 'action/dist/index.cjs'
-})
